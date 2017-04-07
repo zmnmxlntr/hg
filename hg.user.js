3b7a2bdec666cb4266fb9631a178fef42e567f22
zmnmxlntr <zmnmxlntr@outlook.com>
2017-04-07 14:29:06 -0500

    lots of changes

--- hg.user.js
+++ hg.user.js
@@ -4,7 +4,8 @@
 // @include     https://boards.4chan.org/b/thread/*
 // @include     https://boards.4chan.org/trash/thread/*
 // @include     http://brantsteele.net/hungergames/edit.php
-// @version     1.0
+// @version     1.1
+// @downloadURL https://github.com/zmnmxlntr/hg/raw/master/hg.user.js
 // @updateURL   https://github.com/zmnmxlntr/hg/raw/master/hg.user.js
 // @grant       GM_setValue
 // @grant       GM_getValue
@@ -32,7 +33,7 @@ if(window.location.hostname == "boards.4chan.org") {
 
 		var maxLength = 26;
 
-		var x = document.getElementsByClassName("post reply");
+		var x = document.getElementsByClassName("post reply"); // TODO: give a proper name
 
 		for (i = 0; i < x.length; i++) {
 			if(x[i].getElementsByClassName('hg-img').length == 0) {
@@ -45,16 +46,17 @@ if(window.location.hostname == "boards.4chan.org") {
 					var img = y[0].getElementsByTagName("img")[0]['src']
 					var txt = x[i].getElementsByClassName("postMessage")[0].innerText.split('\n')
 					
+					// TODO: wow this is ugly
 					var j = 0;
 					while(j < txt.length && (txt[j].match(/^(>>[0-9]+)(\s\(OP\))?/) || txt[j].trim().length == 0)) {
 						j++;
 					}
 					if(j < txt.length) {
-						txt = txt[j].replace(/[^ú\í\é\ç\'\.\:\-\sa-zA-Z-z0-9]/g, ' ').trim().substring(0, maxLength);
+						txt = txt[j].replace(/[^ú\í\é\ç\'\.\:\-\sa-zA-Z-z0-9]/g, '').trim().substring(0, maxLength);
 					} else {
-						txt = txt.join(' ').replace(/(>>[0-9]+)(\s?\(You\))?(\s?\(OP\))?/g, '').replace(/[^ú\í\é\ç\'\.\:\-\sa-zA-Z-z0-9]/g, ' ').trim().substring(0, maxLength);
+						txt = txt.join(' ').replace(/(>>[0-9]+)(\s?\(You\))?(\s?\(OP\))?/g, '').replace(/[^ú\í\é\ç\'\.\:\-\sa-zA-Z-z0-9]/g, '').trim().substring(0, maxLength);
 					}
-					if(txt.length > 15 && txt.match(/\s/g) == 0) {
+					if(txt.length > 15 && txt.match(/\s/g) == null) {
 						if(txt.length == maxLength) {
 							txt[maxLength - 1] = ' ';
 						} else {
@@ -66,8 +68,8 @@ if(window.location.hostname == "boards.4chan.org") {
 					checkbox.type = "checkbox";
 					checkbox.value = img;
 					checkbox.className = "hg-img";
-					checkbox.id = x[i].getAttribute('id');
-					checkbox.title = "Entry " + entry;
+					checkbox.id = x[i].id; // ?
+					checkbox.title = "Post #" + entry;
 					checkbox.checked = true;
 
 					var text = document.createElement('input')
@@ -75,25 +77,25 @@ if(window.location.hostname == "boards.4chan.org") {
 					text.setAttribute('maxlength', '26')
 					text.setAttribute('size', '36')
 					text.className = "hg-field";
-					text.id = x[i].getAttribute('id');
-					text.title = "Name";
+					text.id = x[i].id; // ?
+					text.title = "Tribute name";
 					text.value = txt;
 
 					var m = document.createElement('input');
-					m.setAttribute('type', 'radio');
-					m.setAttribute('name', 'gender');
-					m.setAttribute('value', 'M');
+					m.type = "radio";
+					m.name = "gender";
+					m.value = "M";
 					m.title = "Male";
 					m.checked = true;
 					var f = document.createElement('input');
-					f.setAttribute('type', 'radio');
-					f.setAttribute('name', 'gender');
-					f.setAttribute('value', 'F');
+					f.type = "radio";
+					f.name = "gender";
+					f.value = "F";
 					f.title = "Female";
 
 					var form = document.createElement('form');
-					form.className = "hg-gender";
-					form.id = x[i].getAttribute('id');
+					form.className = "hg-gender"; // TODO: change this class name
+					form.id = x[i].id; // ?
 
 					form.appendChild(checkbox);
 					form.appendChild(text);
@@ -122,6 +124,14 @@ if(window.location.hostname == "boards.4chan.org") {
 		}
 	};
 
+	function deselect() {
+		var imgs = document.getElementsByClassName("hg-img");
+
+		for(i = 0; i < imgs.length; i++) {
+			imgs[i].checked = false;
+		}
+	};
+
 	function save() {
 		size();
 
@@ -135,8 +145,11 @@ if(window.location.hostname == "boards.4chan.org") {
 		for(i = 0, count = 0; i < imgs.length && count < tributes; i++) {
 			if(imgs[i].checked == true) {
 				count++;
-				imgsStr += imgs[i]['value'] + "|";
-				txtsStr += txts[i]['value'] + "|";
+
+				imgsStr += imgs[i].value + "|";
+				txtsStr += txts[i].value + "|";
+
+				// TODO: handle this garbage in a better way
 				if(gens[i][2].checked == true) {
 					gensStr += "1|";
 				} else {
@@ -155,7 +168,7 @@ if(window.location.hostname == "boards.4chan.org") {
 	document.onkeydown = function(key) {
 		key = key || window.event;
 
-		if(key.keyCode == 112) {
+		if(key.keyCode == 112 || key.keyCode == 115) {
 			draw();
 		} else if(key.keyCode == 113) {
 			hide();
@@ -163,33 +176,36 @@ if(window.location.hostname == "boards.4chan.org") {
 	};
 
 	var t24 = document.createElement('input');
-	t24.setAttribute('type', 'radio');
-	t24.setAttribute('name', 'tributes');
-	t24.setAttribute('value', '24');
+	t24.type = "radio";
+	t24.name = "tributes";
+	t24.value = "24";
 	t24.title = "24 Tributes";
 	t24.checked = true;
 	var t36 = document.createElement('input');
-	t36.setAttribute('type', 'radio');
-	t36.setAttribute('name', 'tributes');
-	t36.setAttribute('value', '36');
+	t36.type = "radio";
+	t36.name = "tributes";
+	t36.value = "36";
 	t36.title = "36 Tributes";
 	var t48 = document.createElement('input');
-	t48.setAttribute('type', 'radio');
-	t48.setAttribute('name', 'tributes');
-	t48.setAttribute('value', '48');
+	t48.type = "radio";
+	t48.name = "tributes";
+	t48.value = "48";
 	t48.title = "48 Tributes";
-
-	var button3 = document.createElement("button");
-	button3.innerHTML = "Save";
-	button3.onclick = function() { save(); };
 
 	var button1 = document.createElement("button");
 	button1.innerHTML = "Draw";
 	button1.onclick = function() { draw(); };
+	var button2 = document.createElement("button");
+	button2.innerHTML = "Save";
+	button2.onclick = function() { save(); };
+	var button3 = document.createElement("button");
+	button3.innerHTML = "Deselect All";
+	button3.onclick = function() { deselect(); };
 
 	var form = document.createElement('div');
 	form.className = "hungergames";
 	form.appendChild(button1);
+	form.appendChild(button2);
 	form.appendChild(button3);
 	form.appendChild(t24);
 	form.appendChild(t36);
@@ -200,25 +216,26 @@ if(window.location.hostname == "boards.4chan.org") {
 	var seasonname = document.getElementsByName("seasonname")[0].value;
 	var logourl = document.getElementsByName("logourl")[0].value;
 
-	var button2 = document.createElement("button");
-	button2.innerHTML = "Load";
-	button2.onclick = function() {
+	var button = document.createElement("button");
+	button.innerHTML = "Load";
+	button.onclick = function() {
 		var tributes = GM_getValue("tributes");
 
 		var imgs = GM_getValue("imgs").split('|');
 		var txts = GM_getValue("txts").split('|');
 		var gens = GM_getValue("gens").split('|');
 
-		var inputs = document.getElementsByTagName('input');
-		for(i = 2, j = 0; i < inputs.length && j < tributes && j < imgs.length - 1; i += 4, j++) {
-			inputs[i]['value'] = txts[j];
-			inputs[i + 2]['value'] = txts[j];
-			inputs[i + 1]['value'] = imgs[j];
-			inputs[i + 3]['value'] = "BW";
-		}
+		var capacity = (document.getElementsByTagName("select").length - 2) / 3
 
+		var inputs = document.getElementsByTagName('input');
+		for(i = 2, j = 0; i < inputs.length && j < tributes && j < capacity && j < imgs.length - 1; i += 4, j++) {
+			inputs[i].value = txts[j];
+			inputs[i + 2].value = txts[j];
+			inputs[i + 1].value = imgs[j];
+			inputs[i + 3].value = "BW";
+		}
 		var genders = document.getElementsByTagName("select");
-		for(i = 1, j = 0; i < tributes * 3 + 1 && j < tributes && j < imgs.length - 1; i += 3, j++) {
+		for(i = 1, j = 0; i < tributes * 3 + 1 && j < tributes && j < capacity && j < imgs.length - 1; i += 3, j++) {
 			genders[i].value = gens[j];
 		}
 
@@ -229,5 +246,5 @@ if(window.location.hostname == "boards.4chan.org") {
 			document.getElementsByName("logourl")[0].value = logourl;
 		}
 	};
-	document.getElementsByTagName("body")[0].prepend(button2);
+	document.getElementsByTagName("body")[0].prepend(button);
 }
